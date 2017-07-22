609f83c841238a4ec8f207d2928bdb79f87bd1cd
Nathan Lewis <nathanlewis42@gmail.com>
2017-07-22 21:02:42 +0100

    fixed Full.js compile problems

--- src/Containers/Full/Full.js
+++ src/Containers/Full/Full.js
@@ -10,42 +10,25 @@ import BuyPolicy from '../../Components/BuyPolicy';
 
 class Full extends Component {
     render() {
-        return ( <
-            div className = "app" >
-            <
-            Header / >
-            <
-            div className = "app-body" >
-            <
-            Sidebar {...this.props }
-            /> <
-            main className = "main" >
-            <
-            Breadcrumb / >
-            <
-            div className = "container-fluid" >
-            <
-            Switch >
-            <
-            Route path = "/Investor"
-            name = "Investor"
-            component = { InvestorDashboard }
-            /> <
-            Route path = "/Client"
-            name = "Client"
-            component = { BuyPolicy }
-            /> <
-            Redirect from = "/"
-            to = "/Investor" / >
-            <
-            /div> <
-            /main> <
-            Aside / >
-            <
-            /div> <
-            Footer / >
-            <
-            /div>
+        return ( 
+            <div className = "app">
+            <Header/>
+            <div className = "app-body"> 
+            <Sidebar {...this.props }/> 
+            <main className = "main">
+            <Breadcrumb/>
+            <div className = "container-fluid">
+            <Switch>
+            <Route path = "/Investor" name = "Investor" component = { InvestorDashboard }/> 
+            <Route path = "/Client" name = "Client" component = { BuyPolicy }/> 
+            <Redirect from = "/" to = "/Investor"/>
+            </Switch>
+            </div> 
+            </main> 
+            <Aside/>
+            </div> 
+            <Footer/>
+            </div>
         );
     }
 }
