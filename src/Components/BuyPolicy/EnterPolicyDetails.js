14e7d5af2356ed209abbe1a8cde4f02cc3f47d1f
yemi-t <toluhi@yahoo.com>
2017-07-23 12:22:07 +0100

    wizard styling

--- src/Components/BuyPolicy/EnterPolicyDetails.js
+++ src/Components/BuyPolicy/EnterPolicyDetails.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 export default class EnterPolicyDetails extends Component{
-    constructor(props){
-        super(props);
-    }
+      constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleSNoChange = this.handleSNoChange.bind(this);
+    this.handleYearChange = this.handleYearChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSNoChange(event) {
+    this.setState({serialNumber: event.target.value});
+  }
+
+  handleYearChange(event) {
+    this.setState({year: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A request has been submitted: ' + this.state.serialNumber + ' ' + this.state.year);
+    event.preventDefault();
+  }
 
     render(){
         if(this.props.policyType === 'mac'){
@@ -12,17 +30,17 @@ export default class EnterPolicyDetails extends Component{
                 <div className="form-group row">
                 <label for="serial-number" className="col-2 col-form-label">Serial Number</label>
                     <div className="col-10">
-                        <input className="form-control" type="text" value="" id="serial-number" placeholder="####-####-####-####"/>
+                        <input className="form-control" type="text" value={this.state.serialNumber} onChange={this.handleSNoChange} id="serial-number" placeholder="####-####-####-####"/>
                     </div>
                 </div>
                 <div className="form-group row">
                 <label for="year" className="col-2 col-form-label">Year of Manufacture</label>
                     <div className="col-10">
-                        <input className="form-control" type="number" value="" id="year" placeholder="YYYY"/>
+                        <input className="form-control" type="number" value={this.state.year} onChange={this.handleYearChange} id="year" placeholder="YYYY"/>
                     </div>
                 </div>
-                <div class="form-group row">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div>
+                <button type="submit" className="btn btn-primary pull-right" onClick={this.handleSubmit}>Request Quotes</button>
                 
                 </div>
             </div>
